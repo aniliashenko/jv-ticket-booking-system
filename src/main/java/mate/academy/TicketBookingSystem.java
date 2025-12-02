8e0c8dc4ce8045b2a5cbd1764938c98e39cb3c9f
azura <windsweptofficial@gmail.com>
2025-12-02 15:43:09 +0100

    solved task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,25 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private Semaphore semaphore;
+    private int totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
+        this.semaphore = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            if (!semaphore.tryAcquire()) {
+                return new BookingResult(user, false, "No seats available.");
+            }
+
+            return new BookingResult(user, true, "Booking successful.");
+        } catch (Exception e) {
+            return new BookingResult(user, false, "Something went wrong for " + user);
+        }
     }
 }
